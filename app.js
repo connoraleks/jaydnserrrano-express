b80d29f00fe5f00e95f8c289b0d56cd4c0ece7b4
connoraleks <connoraleks@gmail.com>
2022-10-21 18:10:50 -0400

    removed clutter

--- app.js
+++ app.js
@@ -1,27 +1,14 @@
-var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-
-var indexRouter = require('./routes/index');
 
 var app = express();
-// for app in local apps folder, app.use(express.static(path.join(__dirname, 'app/build')));
 app.use(express.static(path.join(__dirname, 'jaydnserranofrontend/build')));
 app.use(express.static(path.join(__dirname, 'jaydnserranoadmin/build')));
-
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, 'jaydnserranofrontend/build', 'index.html'));
+});
+app.get('/admin', function(req, res) {
+  res.sendFile(path.join(__dirname, 'jaydnserranoadmin/build', 'index.html'));
 });
 
 // error handler
